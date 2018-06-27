d45db0f7a69eb3b681d645fcc1c61e3780289354
Steve Edson <steve3d50n@gmail.com>
2018-06-27 13:50:01 +0100

    cwd

--- update-check.js
+++ update-check.js
@@ -6,10 +6,17 @@ const p = new Push({
     token: process.env['PUSHOVER_TOKEN'],
 });
 
-let fetchPromise = exec('git fetch');
+let fetchPromise = exec('git fetch', {
+   cwd: __dirname
+});
 
-let localRef = exec('git rev-parse HEAD').then(({ stdout }) => stdout);
-let remoteRef = exec('git rev-parse origin/master').then(({ stdout }) => stdout);
+let localRef = exec('git rev-parse HEAD', {
+   cwd: __dirname
+}).then(({ stdout }) => stdout);
+
+let remoteRef = exec('git rev-parse origin/master', {
+   cwd: __dirname
+}).then(({ stdout }) => stdout);
 
 const updateMessage = {
     title: "Home Assistant Updated",
@@ -22,7 +29,9 @@ fetchPromise.then(() => {
         console.log({refs});
 
         if(refs[0] !== refs[1]) {
-            exec('./update.sh').then(({ stdout }) => {
+            exec('./update.sh', {
+   cwd: __dirname
+}).then(({ stdout }) => {
                 console.log(stdout);
 
                 updateMessage.message = `Updated from ${refs[0]} to ${refs[1]}`;
