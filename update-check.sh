f0fc7d6276165e2a8319b91b8ff3f7e2f7368f54
Steve Edson <steve@travlet.com>
2020-02-04 09:21:57 +0000

    fixes?

--- update-check.sh
+++ update-check.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v $(pwd):/usr/src/app -w /usr/src/app node:lts-alpine "npm install && update-check.js"
+docker run --rm -it -v $(pwd):/usr/src/app -w /usr/src/app node:lts-alpine npm install && update-check.js
