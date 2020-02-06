6b566ba8f52a1ca17b130e02b458da4ae7f30d2b
Steve Edson <steve@travlet.com>
2020-02-06 10:02:00 +0000

    updated update checker, won't restart lovelace changes

--- update-check.js
+++ update-check.js
@@ -1,26 +1,14 @@
 #!/usr/bin/env node
 
-const exec = require('child-process-promise').exec;
+const git = require('simple-git/promise')();
+const path = require('path');
 const Push = require('pushover-notifications');
-
-console.log('env', {
-    user: process.env['PUSHOVER_USER'],
-    token: process.env['PUSHOVER_TOKEN'],
-    url: process.env.HOMEASSISTANT_URL,
-});
+const exec = require('child-process-promise').exec;
 
 const p = new Push({
     user: process.env['PUSHOVER_USER'],
     token: process.env['PUSHOVER_TOKEN'],
 });
-
-let fetchPromise = exec('git fetch --verbose', {
-   cwd: __dirname
-});
-
-let localRef = exec('git rev-parse HEAD', {
-   cwd: __dirname
-}).then(({ stdout }) => stdout);
 
 const updateMessage = {
     title: "Home Assistant Updated",
@@ -28,45 +16,37 @@ const updateMessage = {
     url_title: "Open Home Assistant"
 };
 
-fetchPromise.then(({ stdout }) => {
-    let remoteRef = exec('git rev-parse origin/master', {
-        cwd: __dirname
-    }).then(({ stdout }) => stdout);
+async function checkUpdates() {
+    const update = await git.pull('origin', 'master', {'--no-rebase': null});
+    if(update && update.summary.changes) {
+        if(update.files.length === 1 && update.files[0] === 'ui-lovelace.yaml') {
+            console.log('not restarting lovelace changes');
+            return;
+        }
 
-    console.log('fetch', stdout);
-    return Promise.all([localRef, remoteRef]).then(refs => {
-        console.log({refs});
+        const { stdout } = await exec('./update.sh', {
+            cwd: __dirname
+        });
 
-        if(refs[0] !== refs[1]) {
+        if(process.env['PUSHOVER_USER']) {
+            updateMessage.message = 'Updated Home Assistant: ' + JSON.stringify(update.summary);
 
-            console.log('running update');
+            console.log('sending', updateMessage);
 
-            exec('./update.sh', {
-               cwd: __dirname
-            }).then(({ stdout }) => {
-                console.log('update result', stdout);
-
-                updateMessage.message = `Updated from ${refs[0]} to ${refs[1]}`;
-
-                if(process.env['PUSHOVER_USER']) {
-                    console.log('sending', updateMessage);
-
-                    p.send(updateMessage, function (err, result) {
-                        if (err) {
-                            console.error(err);
-                            throw err;
-                        }
-
-                        console.log('pushover', result);
-
-                        process.exit(0);
-                    });
-                } else {
-                    process.exit(0);
+            p.send(updateMessage, function (err, result) {
+                if (err) {
+                    console.error(err);
+                    throw err;
                 }
+
+                console.log('pushover', result);
+
+                process.exit(0);
             });
         } else {
-            console.log('up to date');
+            process.exit(0);
         }
-    });
-});
+    }
+}
+
+checkUpdates();
