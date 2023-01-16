c70eac6d8194a83301e497d925d7fb0671d5215b
Steve Edson <steve3d50n@gmail.com>
2023-01-16 13:26:49 +0000

    don't restart appdaemon

--- update.sh
+++ update.sh
@@ -2,7 +2,9 @@
 
 git pull
 docker-compose pull
-docker-compose down
-docker-compose up -d --build --remove-orphans
+docker-compose down homeassistant
+docker-compose up -d --build --remove-orphans homeassistant
+docker-compose up -d --build --remove-orphans appdaemon
+docker-compose up -d --build --remove-orphans db
 docker image prune -f
 docker container prune -f
