f7ecc2b84dfb77024ce3693626235d1fe51e4e9a
Steve Edson <steve3d50n@gmail.com>
2024-06-29 17:47:43 +0100

    openwakeword

--- update.sh
+++ update.sh
@@ -9,5 +9,6 @@ docker-compose up -d --build --remove-orphans db
 docker-compose up -d --build --remove-orphans ma
 docker-compose up -d --build --remove-orphans whisper
 docker-compose up -d --build --remove-orphans piper
+docker-compose up -d --build --remove-orphans openwakeword
 docker image prune -f
 docker container prune -f
