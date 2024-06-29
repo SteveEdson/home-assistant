0f46d0f8518364d402f12a5b616d6513fe3d02f6
Steve Edson <steve3d50n@gmail.com>
2024-06-29 17:42:19 +0100

    fixes

--- update.sh
+++ update.sh
@@ -7,5 +7,7 @@ docker-compose up -d --build --remove-orphans homeassistant
 docker-compose up -d --build --remove-orphans appdaemon
 docker-compose up -d --build --remove-orphans db
 docker-compose up -d --build --remove-orphans ma
+docker-compose up -d --build --remove-orphans whisper
+docker-compose up -d --build --remove-orphans piper
 docker image prune -f
 docker container prune -f
